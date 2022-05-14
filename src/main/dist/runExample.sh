4ed87bd954c756053e88bc98bb5fdda98d11a1d4
Mike Starkie <hikemike@gmail.com>
2022-05-14 09:08:35 -0400

    make the fix version the same as infix to avoid confusion and to imply Java 11

--- src/main/dist/runExample.sh
+++ src/main/dist/runExample.sh
@@ -1,1 +1,1 @@
-java -cp antlr4-runtime-4.10.1.jar:fix-2.0.jar:infix-2.2.0.jar:slf4j-api-1.7.7.jar:slf4j-simple-1.7.7.jar com.globalforge.infix.example.ExampleApp
+java -cp antlr4-runtime-4.10.1.jar:fix-2.2.0.jar:infix-2.2.0.jar:slf4j-api-1.7.7.jar:slf4j-simple-1.7.7.jar com.globalforge.infix.example.ExampleApp

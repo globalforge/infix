34fe39fa4a852319d0b0335ee8e0a1645954b689
mstarkie <hikemike@gmail.com>
2023-05-30 14:34:59 -0400

    Fix comparison of tagNum which was using string comparison istead of integer comparison

--- src/main/java/com/globalforge/infix/FixDataTagNumComparator.java
+++ src/main/java/com/globalforge/infix/FixDataTagNumComparator.java
@@ -4,12 +4,19 @@ import java.util.Comparator;
 
 /**
  * This will sort FIX data instances by tag number (e.g., 35, 150, etc.).
+ * 
  * @author mstarkie
  *
  */
 public class FixDataTagNumComparator implements Comparator<FixData> {
-   @Override
-   public int compare(FixData o1, FixData o2) {
-      return o1.getTagNum().compareTo(o2.getTagNum());
-   }
+	@Override
+	public int compare(FixData o1, FixData o2) {
+		try {
+			Integer o1TagNum = Integer.valueOf(o1.getTagNum());
+			Integer o2TagNum = Integer.valueOf(o2.getTagNum());
+			return o1TagNum.compareTo(o2TagNum);
+		} catch (NumberFormatException e) {
+			return o1.getTagNum().compareTo(o2.getTagNum());
+		}
+	}
 }
